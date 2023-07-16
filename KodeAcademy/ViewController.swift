50be831e7734a7ba976458a250fd209a58501ebc
05Waleed <139551937+05Waleed@users.noreply.github.com>
2023-07-16 14:49:07 +0200

    update

--- KodeAcademy/ViewController.swift
+++ KodeAcademy/ViewController.swift
@@ -9,9 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var nameField: UITextField!
-    @IBOutlet weak var passField: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
