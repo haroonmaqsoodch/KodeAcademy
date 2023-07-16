dd93d208c5c56237d55106557ba0b71c9534da42
05Waleed <139551937+05Waleed@users.noreply.github.com>
2023-07-16 14:56:08 +0200

    update all

--- KodeAcademy/ViewController.swift
+++ KodeAcademy/ViewController.swift
@@ -9,21 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var numArray: [Int] = [1,2,3,4,5,6,7,8,9,10]
+    var sum = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        divide()
+        sumOfArray()
     }
     
     func divide() {
         print(4 / 2)
     }
-    
-    // zuhair
-    
+        
     func substract(num:Int,numTwo:Int){
         print(num-numTwo)
     }
+        
+    func sumOfArray() {
+        for i in numArray {
+            sum += i
+        }
+        print(sum)
+    }
+    
+    func checkColour() {
+        var colour = "Blue"
+        
+        if colour == "Blue" {
+            print("yess")
+        } else {
+            print("no")
+        }
+    }
+
+    func car() {
+        var carName = "pata nahi"
+        
+        if carName == "BMW" {
+            print("hehe")
+        } else {
+            "nooooo"
+        }
+    }
+    
 }
 
 
